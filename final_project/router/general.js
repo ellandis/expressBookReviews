52d70a7e577ee03c9344cd1d8ebab4fea7ef1857
ellandis <elamarlandis@gmail.com>
2026-01-25 14:17:58 -0500

    get-isbn port-8000

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,7 +21,16 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if(book){
+        res.send(book);
+        res.status(200).json(book);
+    }
+    else{
+        res.status(400).json({message:"Book not found"});
+    }
+
  });
   
 // Get book details based on author

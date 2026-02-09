4e3d3f8b39231c14bea76451c06fbbd4138e9c6a
ellandis <elamarlandis@gmail.com>
2026-02-09 16:17:26 -0500

    adding async/wait

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,81 +5,68 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-
-const authenticatedUser = (username, password) => {
-    let validUser = users.filter((user) => {
-        return (user.username === username && user.password === password);
-    });
-    if(validUser.length > 0){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-const doesExist = (username) => {
-    let userWithSameName = users.filter((u) => {
-    return u.username === username});
-
-    if(userWithSameName.length > 0){
-        return true;
-    }
-    else {
-        return false
-    }
-}
-
-
-
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", async (req,res) => {
   //Write your code here
-    const userName = req.body.username;
-    const passWord = req.body.password;
+    try {
+        const userName = req.body.username;
+        const passWord = req.body.password;
 
-    if(userName && passWord){
-        if(!doesExist(userName)){
-            users.push({"username":userName, "password":passWord});
-                return res.status(200).json({message: "User successfully registered. Now you can login"});
+        if(!userName || !passWord){
+            return res.status(400).json({message: "Username and password Required"});
         }
-        else{
-            return res.status(404).json({message: "User already exists!"});
-        }
-    }
-    else{
-        if(userName){
-            return res.send({message:"Password was not provided"});
-        }
-        else if(passWord){
-            return res.send({message:"Username was not provided"});
-        }
-        else{
-            return res.send({message:"Username and password was not provided"});
-        }
-    }
 
+        const userExists = await checkUserExists(userName);
+
+        if(userExists){
+        return res.status(409).json({message: "User already exist"});
+        }
+
+        await createUser({userName,passWord});
+        return res.status(201).json({message:"User successfully register. Now you can login"});
+    }
+    catch(error){
+        console.error("Registration error: ", error);
+        return res.status(500).json({message:"Registration failed", error: error.message});
+    }
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-      res.send(JSON.stringify(books,null,4));
+public_users.get('/', async (req, res) => {
+    try {
+        const bookslist = await getBooks();
+        return res.status(200).send(JSON.stringify(bookslist,null,4));
+    }
+    catch(error){
+        return res.status(500).json({
+            message: "Error retrieving books",
+            error: error.message
+        });
+    }
 
-//  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-    const isbn = req.params.isbn;
-    const book = books[isbn];
-    if(book){
-        res.send(JSON.stringify(book,null,4));
+public_users.get('/isbn/:isbn', async (req, res) => {
+    try{
+        const isbn = req.params.isbn;
+        const book = await getBookISBN(isbn);
+
+        if (!book) {
+            return res.status(404).json({
+            message: "Book not found"
+            });
+        }
+        return res .status(200).send(JSON.stringify(book,null,4));
     }
-    else{
-        res.status(400).json({message:"Book not found"});
+    catch(error){
+        return res.status(404).json({
+            message: "Book not found",
+            error: error.message
+        });
     }
  });
+
+
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -118,5 +105,68 @@ public_users.get('/review/:isbn',function (req, res) {
 
   res.send(JSON.stringify(review,null,4));
 });
+
+
+
+/*
+**********************************************
+helper functions
+**********************************************
+*/
+
+const authenticatedUser = (username, password) => {
+    let validUser = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    if(validUser.length > 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+const doesExist = (username) => {
+    let userWithSameName = users.filter((u) => {
+    return u.username === username});
+
+    if(userWithSameName.length > 0){
+        return true;
+    }
+    else {
+        return false
+    }
+}
+
+function checkUserExists(username) {
+    return new Promise((resolve,reject) => {
+        setTimeout( () => { resolve(doesExist(username)); }, 100);
+    });
+}
+
+function createUser(userData){
+    return new Promise((resolve,reject) => {
+        setTimeout( () => users.push(userData), resolve(userData), 100);
+    });
+}
+
+function getBookISBN(isbn){
+    return new Promise( (resolve,reject) => {
+        const book = books[isbn];
+        if(book){ resolve(book);}
+        else{ resolve(null);}
+    });
+}
+
+const getBooks = () => {
+    return new Promise ((resolve,reject) => {
+        if(books) {
+            resolve(books);
+        }
+        else{
+            reject(new Error("Books not found."));
+        }
+    });
+}
+
 
 module.exports.general = public_users;

37db1edaccb567ebe125ba29f6739361c14f68b3
ellandis <elamarlandis@gmail.com>
2026-02-10 16:06:52 -0500

    JS is top down read helper func moved back to top find by author done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,6 +5,83 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
+const authenticatedUser = (username, password) => {
+    let validUser = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    if(validUser.length > 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+const doesExist = (username) => {
+    let userWithSameName = users.filter((u) => {
+    return u.username === username});
+
+    if(userWithSameName.length > 0){
+        return true;
+    }
+    else {
+        return false
+    }
+}
+
+function checkUserExists(username) {
+    return new Promise((resolve,reject) => {
+        setTimeout( () => { resolve(doesExist(username)); }, 100);
+    });
+}
+
+function createUser(userData){
+    return new Promise((resolve,reject) => {
+        setTimeout( () => users.push(userData), resolve(userData), 100);
+    });
+}
+
+function getBookISBN(isbn){
+    return new Promise( (resolve,reject) => {
+        const book = books[isbn];
+        if(book){ resolve(book);}
+        else{ resolve(null);}
+    });
+}
+
+const getBooks = () => {
+    return new Promise ((resolve,reject) => {
+        if(books) {
+            resolve(books);
+        }
+        else{
+            reject(new Error("Books not found."));
+        }
+    });
+    }
+
+function getBooksByAuthor(authorName){
+        return new Promise( (resolve,reject) => {
+            try {
+                const booksByAuthor = [];
+
+                for (let isbn in books){
+                    if(books[isbn].author.toLowerCase() === authorName.toLowerCase()){
+                        booksByAuthor.push({
+                            isbn: isbn,
+                            ...books[isbn]
+                        });
+                    }
+                }
+                resolve(booksByAuthor);
+            }
+            catch(error){
+                reject(error);
+            }
+        });
+    }
+
+
+
 public_users.post("/register", async (req,res) => {
   //Write your code here
     try {
@@ -65,15 +142,13 @@ public_users.get('/isbn/:isbn', async (req, res) => {
     }
  });
 
-
-
 // Get book details based on author
 public_users.get('/author/:author',async (req, res) => {
     try{
         const authorName = req.params.author;
         const allBooks = await getBooksByAuthor(authorName);
 
-        if(!allBooks || booksByAuthor.length === 0){
+        if(!allBooks || allBooks.length === 0){
             return res.status(404).json({message: "No books found for this author"})
         }
 
@@ -82,24 +157,6 @@ public_users.get('/author/:author',async (req, res) => {
             res.status(500).json({message:"Book not found", error: error.message});
     }
 });
-
-function getBooksByAuthor(authorName){
-    return new Promise( (resolve,reject) => {
-        try {
-            const booksByAuthor = {};
-
-            for (let isbn in books){
-                if(books[isbn].author.toLowerCase() === authorName.toLowerCase()){
-                    booksByAuthor[isbn] = books[isbn];
-                }
-            }
-            resolve(booksByAuthor);
-        }
-        catch(error){
-            reject(error);
-        }
-    });
-}
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
@@ -130,59 +187,6 @@ helper functions
 **********************************************
 */
 
-const authenticatedUser = (username, password) => {
-    let validUser = users.filter((user) => {
-        return (user.username === username && user.password === password);
-    });
-    if(validUser.length > 0){
-        return true;
-    }else{
-        return false;
-    }
-}
-
-const doesExist = (username) => {
-    let userWithSameName = users.filter((u) => {
-    return u.username === username});
-
-    if(userWithSameName.length > 0){
-        return true;
-    }
-    else {
-        return false
-    }
-}
-
-function checkUserExists(username) {
-    return new Promise((resolve,reject) => {
-        setTimeout( () => { resolve(doesExist(username)); }, 100);
-    });
-}
-
-function createUser(userData){
-    return new Promise((resolve,reject) => {
-        setTimeout( () => users.push(userData), resolve(userData), 100);
-    });
-}
-
-function getBookISBN(isbn){
-    return new Promise( (resolve,reject) => {
-        const book = books[isbn];
-        if(book){ resolve(book);}
-        else{ resolve(null);}
-    });
-}
-
-const getBooks = () => {
-    return new Promise ((resolve,reject) => {
-        if(books) {
-            resolve(books);
-        }
-        else{
-            reject(new Error("Books not found."));
-        }
-    });
-}
 
 
 module.exports.general = public_users;
